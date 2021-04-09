047d92efc971ca8ede68198df1cc70b42df80ebe
Ilya Sereda <ilyasereda141@gmail.com>
2021-04-09 12:04:06 +0300

    Fix2

--- src/test/java/com/sd/SummaChisel.java
+++ src/test/java/com/sd/SummaChisel.java
@@ -4,7 +4,7 @@ public class SummaChisel {
     public static void main(String[] args) {
 
         int z = 2147483647;
-        z = z + (- z * 2 - 1);
+        z = z * (-1) - 1;
         System.out.println(z);
 
     }
